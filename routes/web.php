9221f448bd22e9c096ba718e567231232f71c15d
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-07-27 13:38:32 +0300

    All routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandscapeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PlantController;
+use App\Http\Controllers\PotController;
 
-Route::get('/', [LandscapeController::class, 'index'])->name('home');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/services', [PageController::class, 'services'])->name('services');
+Route::get('/our-work', [PageController::class, 'work'])->name('work');
+Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
+Route::get('/plants/{category}', [PlantController::class, 'category'])->name('plants.category');
+Route::get('/pots', [PotController::class, 'index'])->name('pots');
+Route::get('/areas-we-serve', [PageController::class, 'areas'])->name('areas');
+Route::get('/about', [PageController::class, 'about'])->name('about');
