a1a258bae0995c7327d892070aae5b979159f729
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-09-22 10:22:34 +0300

    add routes to show plant and pot

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/services', [PageController::class, 'services'])->name('services');
 Route::get('/our-work', [PageController::class, 'work'])->name('work');
 Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
 Route::get('/plants/{category}', [PlantController::class, 'category'])->name('plants.category');
-Route::get('/pots', [PotController::class, 'index'])->name('pots');
+Route::get('/pots', [PotController::class, 'index'])->name('pots.index');
 Route::get('/areas-we-serve', [PageController::class, 'areas'])->name('areas');
 Route::get('/about', [PageController::class, 'about'])->name('about');
+
+Route::get('/pots/{pot:slug}', [PotController::class, 'show'])->name('pots.show');
+Route::get('/plant/{plant:slug}', [PlantController::class, 'show'])->name('plants.show');
